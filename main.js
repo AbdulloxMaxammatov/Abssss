84dd451656936481e832a8251dcc2443c53a2079
Abdullox <maxammatovabdullox27@gmail.com>
2024-02-11 16:26:15 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// let arrnum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// Array.prototype.sum = function () {
+//     let sum = 0;
+
+//     for (let i = 0; i < this.length; i++) {
+//         if (typeof this[i] === "number") {
+//             sum += this[i];
+//         } else {
+//             return "Error";
+//         }
+//     }
+//     return sum;
+// }
+// console.log(arrnum);
+
+
+
+// let arrStr = ["A", "B", "D", "C", "D", "F", "G","H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+// Array.prototype.sortStringAZ = function () {
+//     let arr = [];
+//     arr = this;
+//     arr.sort();
+
+//     return arr;
+// }
+
+
+// // Array.prototype.sortStringZA = function () {
+// //     let arr = [];
+// //     arr = this;
+// //     arr.sort((a, b) => {
+// //         return b.localeCompare(a);
+// //     });
+
+// //     return arr;
+// // }
+
+// console.log(arrStr.sortStringZA());
